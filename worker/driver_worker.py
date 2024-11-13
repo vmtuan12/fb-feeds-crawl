51f7df483aca3680238053cd7fbf71a5bf935415
Vu Minh Tuan <tuanvm@dagoras.io>
2024-11-14 00:59:39 +0700

    refactor base driver worker

--- worker/driver_worker.py
+++ worker/driver_worker.py
@@ -2,5 +2,5 @@ from worker.base_worker import BaseWorker
 from driver.custom_driver import CustomDriver
 
 class DriverWorker(BaseWorker):
-    def __init__(self) -> None:
-        self.driver = CustomDriver()
+    def __init__(self, driver) -> None:
+        self.driver = driver
