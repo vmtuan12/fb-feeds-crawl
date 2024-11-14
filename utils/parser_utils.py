a12ea41a87285bb806f5327dec7653e7d68a8e84
Vu Minh Tuan <tuanvm@dagoras.io>
2024-11-15 00:25:27 +0700

    modify parser utils

--- utils/parser_utils.py
+++ utils/parser_utils.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 
 class ParserUtils():
     @classmethod
-    def match_text_and_number(text: str) -> str | None:
+    def match_text_and_number(cls, text: str) -> str | None:
         pattern = r'[0-9a-zA-Z]+'
         match = re.search(pattern, text)
         if match:
@@ -12,9 +12,13 @@ class ParserUtils():
         return None
     
     @classmethod
-    def approx_post_time_str(now: datetime, post_time: str) -> str | None:
-        if post_time == "Just now":
-            return str(now)
+    def approx_post_time_str(cls, now: datetime, raw_post_time: str) -> str | None:
+        post_time = cls.match_text_and_number(raw_post_time)
+        if post_time == None:
+            return None
+        
+        if post_time.startswith("Jus"):
+            return now.strftime("%Y-%m-%d %H:%M:%S")
         
         time_int = int(post_time[:-1])
         unit = post_time[-1]
@@ -24,15 +28,16 @@ class ParserUtils():
         elif unit == "h":
             real_post_time = now - timedelta(hours=time_int)
 
-        return str(real_post_time)
+        return real_post_time.strftime("%Y-%m-%d %H:%M:%S")
     
     @classmethod
-    def approx_reactions(reactions: str) -> int:
+    def approx_reactions(cls, reactions: str) -> int:
         last_word = reactions[-1]
         if not ('0' <= last_word and last_word <= '9'):
+            number_part = reactions[:-1].replace(",", ".")
             if last_word.lower() == 'k':
-                return int(reactions[:-1]) * 1000
+                return int(float(number_part) * 1000)
             elif last_word.lower() == 'm':
-                return int(reactions[:-1]) * 1000000
+                return int(float(number_part) * 1000000)
         else:
             return int(reactions)
