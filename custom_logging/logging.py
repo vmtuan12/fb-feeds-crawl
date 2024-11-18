03983f671321f146a30865cad1190e87db63f0a6
Vu Minh Tuan <tuanvm@dagoras.io>
2024-11-19 00:33:00 +0700

    logging

--- custom_logging/logging.py
+++ custom_logging/logging.py
@@ -2,15 +2,47 @@ import logging
 from datetime import datetime
 import pytz
 
+class CustomFormatter(logging.Formatter):
+    def __init__(self, *args, timezone='UTC', **kwargs):
+        super().__init__(*args, **kwargs)
+        self.timezone = pytz.timezone(timezone)
+
+    def formatTime(self, record, datefmt=None):
+        return super().formatTime(record, datefmt="%Y-%m-%d %H:%M:%S")
+
+class TerminalLogging():
+
+    @staticmethod
+    def log_info(message: str):
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(CustomFormatter('%(asctime)s %(levelname)s: %(message)s', timezone='Asia/Ho_Chi_Minh'))
+        logging.basicConfig(level=logging.DEBUG, handlers=[console_handler])
+        logging.info(message)
+
+    @staticmethod
+    def log_error(message: str):
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(CustomFormatter('%(asctime)s %(levelname)s: %(message)s', timezone='Asia/Ho_Chi_Minh'))
+        logging.basicConfig(level=logging.DEBUG, handlers=[console_handler])
+        logging.error(message)
+
 class FileLogging():
     @staticmethod
     def log_info(file_path: str, message: str):
-        logging.basicConfig(filename=file_path,level=logging.DEBUG)
-        now = datetime.now(pytz.timezone('Asia/Ho_Chi_Minh')).strftime("%Y-%m-%d %H:%M:%S")
-        logging.info(f"{now} --- {message}")
+        handler = logging.FileHandler(file_path)
+        handler.setFormatter(CustomFormatter('%(asctime)s %(levelname)s: %(message)s', timezone='Asia/Ho_Chi_Minh'))
+        logging.basicConfig(
+            level=logging.INFO,
+            handlers=[handler]
+        )
+        logging.info(message)
 
     @staticmethod
     def log_error(file_path: str, message: str):
-        logging.basicConfig(filename=file_path,level=logging.DEBUG)
-        now = datetime.now(pytz.timezone('Asia/Ho_Chi_Minh')).strftime("%Y-%m-%d %H:%M:%S")
-        logging.error(f"{now} --- {message}")
+        handler = logging.FileHandler(file_path)
+        handler.setFormatter(CustomFormatter('%(asctime)s %(levelname)s: %(message)s', timezone='Asia/Ho_Chi_Minh'))
+        logging.basicConfig(
+            level=logging.ERROR,
+            handlers=[handler]
+        )
+        logging.error(message)
