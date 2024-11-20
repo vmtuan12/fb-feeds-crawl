366ba948b28a7112439d64edcb6b3c79882948f7
Vu Minh Tuan <tuanvm@dagoras.io>
2024-11-21 00:03:23 +0700

    add avro

--- connectors/db_connector.py
+++ connectors/db_connector.py
@@ -1,6 +1,9 @@
 import psycopg2
 from kafka import KafkaConsumer, KafkaProducer
 import json
+import io
+import avro.schema
+from avro.io import DatumWriter, BinaryEncoder, BinaryDecoder, DatumReader
 
 class DbConnector():
     def __init__(self, host: str, port: int, 
@@ -96,13 +99,27 @@ class KafkaConsumerBuilder():
         self.auto_offset_reset = auto_offset_reset
         return self
     
-    def build(self):
+    def build(self, avro_schema_path: str | None = None):
+        if avro_schema_path != None:
+            schema = avro.schema.parse(open(avro_schema_path).read())
+        else:
+            schema = None
+        
+        def avro_deserialize(msg):
+            bytes_reader = io.BytesIO(msg)
+            decoder = BinaryDecoder(bytes_reader)
+            reader = DatumReader(schema)
+            return reader.read(decoder)
+        
+        def json_deserialize(msg):
+            return json.loads(msg.decode('utf-8'))
+        
         return KafkaConsumer(self.topics,
                             group_id=self.group_id,
                             bootstrap_servers=self.brokers,
                             enable_auto_commit=self.enable_auto_commit,
                             auto_offset_reset=self.auto_offset_reset,
-                            value_deserializer=lambda x: json.loads(x.decode('utf-8')))
+                            value_deserializer=(json_deserialize if avro_schema_path == None else avro_deserialize))
     
 class KafkaProducerBuilder():
     def __init__(self):
@@ -112,6 +129,22 @@ class KafkaProducerBuilder():
         self.brokers = brokers
         return self
     
-    def build(self):
+    def build(self, avro_schema_path: str | None = None):
+        if avro_schema_path != None:
+            schema = avro.schema.parse(open(avro_schema_path).read())
+        else:
+            schema = None
+
+        def avro_serialize(msg):
+            writer = DatumWriter(schema)
+            bytes_writer = io.BytesIO()
+            encoder = BinaryEncoder(bytes_writer)
+            writer.write(msg, encoder)
+            raw_bytes = bytes_writer.getvalue()
+            return raw_bytes
+        
+        def json_serialize(msg):
+            return json.dumps(msg).encode('utf-8')
+        
         return KafkaProducer(bootstrap_servers=self.brokers, 
-                        value_serializer=lambda x: json.dumps(x).encode('utf-8'))
+                        value_serializer=(json_serialize if avro_schema_path == None else avro_serialize))
