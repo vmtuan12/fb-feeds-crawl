e768061aa1fb9ee0e3246e2ec81b1952769b411c
Vu Minh Tuan <tuanvm@dagoras.io>
2024-11-20 17:02:20 +0700

    offset reset

--- connectors/db_connector.py
+++ connectors/db_connector.py
@@ -74,6 +74,7 @@ class KafkaConsumerBuilder():
         self.brokers = []
         self.group_id = "default"
         self.enable_auto_commit = True
+        self.auto_offset_reset = 'latest'
 
     def set_topics(self, topics: str | list[str]):
         self.topics = topics
@@ -90,12 +91,17 @@ class KafkaConsumerBuilder():
     def set_enable_auto_commit(self, enable_auto_commit: bool):
         self.enable_auto_commit = enable_auto_commit
         return self
+
+    def set_auto_offset_reset(self, auto_offset_reset: str):
+        self.auto_offset_reset = auto_offset_reset
+        return self
     
     def build(self):
         return KafkaConsumer(self.topics,
                             group_id=self.group_id,
                             bootstrap_servers=self.brokers,
                             enable_auto_commit=self.enable_auto_commit,
+                            auto_offset_reset=self.auto_offset_reset,
                             value_deserializer=lambda x: json.loads(x.decode('utf-8')))
     
 class KafkaProducerBuilder():
