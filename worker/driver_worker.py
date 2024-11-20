e09aeb5259e360800bc1170f3db11822f0602170
Vu Minh Tuan <tuanvm@dagoras.io>
2024-11-20 11:56:27 +0700

    clear cache

--- worker/driver_worker.py
+++ worker/driver_worker.py
@@ -1,6 +1,7 @@
 from worker.base_worker import BaseWorker
 from driver.custom_driver import CustomDriver, CustomUCDriver
 from kafka import KafkaProducer
+from time import sleep
 
 class DriverWorker(BaseWorker):
     def __init__(self, driver: CustomDriver | CustomUCDriver) -> None:
@@ -15,11 +16,12 @@ class DriverWorker(BaseWorker):
         # self.kafka_producer = KafkaProducer()
 
     def _clear_cache(self):
-        self.driver.get('chrome://settings/clearBrowserData')
         clear_data_path = "document.querySelector('settings-ui').shadowRoot.querySelector('#main').shadowRoot.querySelector('settings-basic-page').shadowRoot.querySelector('settings-section[section=\"privacy\"]').querySelector('settings-privacy-page').shadowRoot.querySelector('settings-clear-browsing-data-dialog').shadowRoot.querySelector('cr-dialog').querySelector('#clearButton')"
-        self.driver.execute_script(f"return {clear_data_path}").click()
-        self.driver.execute_script('localStorage.clear();')
+        self.driver.get('chrome://settings/clearBrowserData')
+        sleep(1)
+        self.driver.execute_script("arguments[0].click();", self.driver.execute_script(f"return {clear_data_path}"))
         self.driver.delete_all_cookies()
+        self.driver.execute_script('localStorage.clear();')
 
     def _check_ready(self) -> bool:
         return True
