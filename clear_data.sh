05fc107689f33013a8f16dc9ccbb78922b28c5e6
Vu Minh Tuan <tuanvm@dagoras.io>
2024-11-25 15:48:25 +0700

    add options to clear data

--- clear_data.sh
+++ clear_data.sh
@@ -5,3 +5,4 @@ rm -rf "$base_dir"/*/component_crx_cache/*
 rm -rf "$base_dir"/*/BrowserMetric*
 rm -rf "$base_dir"/*/optimization_guide_model_store/*
 rm -rf "$base_dir"/*/Default/optimization_guide_model_store/*
+rm -rf "$base_dir"/*/Default/Cache/*
