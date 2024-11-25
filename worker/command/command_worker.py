aa704111bf23db88f4dd6c6771576da0f87500de
Vu Minh Tuan <tuanvm@dagoras.io>
2024-11-25 17:45:31 +0700

    command worker

--- worker/command/command_worker.py
+++ worker/command/command_worker.py
@@ -18,13 +18,51 @@ class CommandWorker(BaseWorker):
                                                     .build()
         
     def _get_page_list(self) -> list:
-        return []
+        cursor = self.pg_conn.cursor()
+        cursor.execute("""
+        SELECT * FROM (
+        SELECT pl.username FROM fb.page_lang pl LEFT JOIN fb.facebook_page fp ON pl.pageid = fp.pageid
+        WHERE pl.name_lang = 'vi' AND pl.about_lang = 'vi' AND pl.des_lang = 'vi' AND pl.username != 'NaN'
+        AND pl.username NOT IN (SELECT po.username from fb.page_options po WHERE po.option = 'excluded')
+        AND fp.categories && ARRAY[
+            'Trang web tin tức & truyền thông',
+            'Công ty truyền thông/tin tức',
+            'Giải trí',
+            'Công ty truyền thông xã hội',
+            'Nghệ thuật & Giải trí',
+            'Công ty truyền thông & sản xuất truyền thông',
+            'Entertainment Website',
+            'Báo',
+            'Tạp chí',
+            'Trang web giải trí',
+            'Chuyên gia tin tức'
+            ]
+        AND NOT (fp.categories && ARRAY['Nghệ sỹ', 'Diễn viên hài', 'Diễn viên', 'Cửa hàng nội thất', 'Cửa hàng quần áo sơ sinh & trẻ em', 'Người của công chúng'])
+        AND pl.numlikes >= 10000
+        ORDER BY pl.numlikes DESC
+        LIMIT 1000) p1
+        UNION
+        (SELECT po.username from fb.page_options po WHERE po.option = 'included');
+        """)
+        rows = cursor.fetchall()
+        cursor.close()
+        return set([r[0] for r in rows])
 
     def start(self):
         list_pages = self._get_page_list()
+        current_partition = 0
+
         for index, p in enumerate(list_pages):
             scrape_msg = CommandEntity(cmd_type=CommandType.SCRAPE_PAGE, page=p).to_dict()
-            self.kafka_producer.send(Kafka.TOPIC_COMMAND, value=scrape_msg)
+
+            try:
+                self.kafka_producer.send(Kafka.TOPIC_COMMAND, value=scrape_msg, partition=current_partition)
+            except AssertionError as ae:
+                current_partition = 0
+                self.kafka_producer.send(Kafka.TOPIC_COMMAND, value=scrape_msg, partition=current_partition)
+
+            current_partition += 1
+
             TerminalLogging(f"Command {scrape_msg}")
 
             if index > 0 and (index % 10 == 0 or index == (len(list_pages) - 1)):
@@ -32,3 +70,14 @@ class CommandWorker(BaseWorker):
                 TerminalLogging(f"Command {clear_cache_msg}")
                 
         self.kafka_producer.flush()
+
+    def clean_up(self):
+        self.kafka_producer.flush()
+        self.kafka_producer.close(timeout=5)
+        self.pg_conn.close()
+
+    def __del__(self):
+        self.clean_up()
+    
+    def __delete__(self):
+        self.clean_up()
