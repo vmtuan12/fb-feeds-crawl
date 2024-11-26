8093826cb350eef002412e5d274c6606c7f916c0
vmtuan12 <100104530+vmtuan12@users.noreply.github.com>
2024-11-26 23:24:13 +0700

    Update constants_example.py

--- utils/constants_example.py
+++ utils/constants_example.py
@@ -1,11 +1,23 @@
+import os
+
 class PostgresConnectionConstant():
-    HOST = ""
+    HOST = "localhost"
     PORT = 5432
-    USER = ""
-    PWD = ""
-    DB = ""
+    USER = "tuanvm"
+    PWD = "tuanvm"
+    DB = "tuanvm_db"
 
 class KafkaConnectionConstant():
     BROKERS = [
         "localhost:9091"
     ]
+    GROUP_ID_MIDDLE_CONSUMER = "MidCon1"
+    TOPIC_RAW_POST = "fb.raw_post"
+    TOPIC_COMMAND = "fb.command"
+    TOPIC_DEAD_PAGES = "fb.dead_pages"
+    TOPIC_RECHECK_PAGES = "fb.recheck_pages"
+
+class SysConstant():
+    BASE_DIR = os.getcwd()
+    USER_DATA_DIR = f"{BASE_DIR}/chrome_profiles"
+    CLEAR_DATA_CHROME_SCRIPT = f"{BASE_DIR}/clear_data.sh"
