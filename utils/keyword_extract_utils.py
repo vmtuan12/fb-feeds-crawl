6e9c9985ec00dfa3d6bf44dcde14ddc7e0067b62
Vu Minh Tuan <tuanvm@dagoras.io>
2024-12-02 16:01:01 +0700

    extract keywords v2

--- utils/keyword_extract_utils.py
+++ utils/keyword_extract_utils.py
@@ -9,50 +9,59 @@ from custom_logging.logging import TerminalLogging
 
 class KeywordExtractionUtils():
     BASE_PROMPT = """
-    Given the paragraphs, each is put between 2 backticks
+    Given the paragraphs in format json, with key is the ID of the paragraph, and value is the text
     {0}
     You are an experienced writer. Remove all stopwords and words that means nothing to the event, and extract the most important keywords in the above paragraphs that describe the event IN VIETNAMESE.  Result of each paragraph is answered on a new line, with the same order. No explaination, no comments, no analyzing, no note, you say nothing. Just do as I say. Write me the answer only with the format like
-    [
-    "keyword1,keyword2,keyword3",
-    "keyword1,keyword2,keyword3",
-    "keyword1,keyword2,keyword3"
-    ]
-    Write in json, result has the same order as input, which means the first paragraph corresponds to the first line of keywords, the second paragraph corresponds to the second line of keywords, etc. Remember: time is not a keyword. Name of a place or people is automatically a keyword. Keywords must be contained in the paragraph, do not change or paraphrase them. Do not skip any paragraph. Ensure the there must be exactly {1} lines of keywords.
+    {1}
+    Write in json, keywords of a paragraph is assigned with its ID. 
+    Remember: time is not a keyword. Name of a place or people is automatically a keyword. Word or complex words that describes what happens is automatically a keyword. Verb of complex verbs or that describes action of people is automatically a keyword
+    Do not skip any paragraph. 
+    Keywords must exist in the paragraph, do not change or paraphrase them.
+    You are prohibited from changing the key.
+    Ensure the there are exactly {2} elements of keywords
     """
 
     @classmethod
     def enrich_keywords(cls, list_data: list[dict], api_key: str, model_name="gemini-1.5-flash") -> list:
         while (True):
             try:
-                list_text_str = ""
-                for item in list_data:
-                    list_text_str += f"`{ParserUtils.strip_emoji(item.get('text'))}`;\n"
+                start_time = time.time()
+                post_by_id_dict = {}
+                list_text_str = "{"
 
-                prompt = cls.BASE_PROMPT.format(list_text_str, len(list_data))
+                for index, item in enumerate(list_data):
+                    post_by_id_dict[str(index)] = item
+                    list_text_str += f'"{str(index)}": "{ParserUtils.strip_emoji(item.get("text"))}",'
+
+                list_text_str += list_text_str[:-1] + "}"
+
+                prompt = cls.BASE_PROMPT.format(list_text_str, '{id1: "keyword1,keyword2,keyword3", id2: "keyword1,keyword2,keyword3", id3: "keyword1,keyword2,keyword3"}', len(list_data))
 
                 genai.configure(api_key=api_key)
-                model = genai.GenerativeModel(model_name)
+                model = genai.GenerativeModel("gemini-1.5-flash")
 
                 TerminalLogging.log_info(f"Extracting keywords with API KEY {api_key}")
                 response = model.generate_content(prompt)
                 
                 result_str = response.text
-                list_keywords_by_post = json.loads(result_str.replace("```json", "").replace("```", "").replace("\n", ""))
+                dict_keywords_by_post = json.loads(result_str.replace("```json", "").replace("```", "").replace("\n", ""))
 
-                if len(list_data) != len(list_keywords_by_post):
-                    print(len(list_data), len(list_keywords_by_post))
-                    raise KeywordsNotMatchedException(msg="Different in length")
+                # print(len(dict_keywords_by_post.keys()))
+                if len(post_by_id_dict.keys()) != len(dict_keywords_by_post.keys()):
+                    [print(post_by_id_dict[item]["text"]) for item in post_by_id_dict]
+                    print(dict_keywords_by_post)
+                    raise KeywordsNotMatchedException("Length fail")
                 
-                for index, item in enumerate(list_data):
-                    keyword_list = [k.lower() for k in list_keywords_by_post[index].split(",")]
+                print(dict_keywords_by_post.keys(), post_by_id_dict.keys())
+                for key in dict_keywords_by_post.keys():
+                    if dict_keywords_by_post.get(key) == None:
+                        dict_keywords_by_post[key] = ""
+                    keyword_list = [k.strip().lower() for k in dict_keywords_by_post[key].split(",")]
+                    post_by_id_dict[key]["keywords"] = keyword_list
 
-                    if (keyword_list[0] not in item.get('text').lower()) and (keyword_list[-1] not in item.get('text').lower()):
-                        TerminalLogging.log_info(f"{keyword_list[0]}, {keyword_list[-1]}\n{item.get('text').lower()}")
-                        raise KeywordsNotMatchedException(msg="Wrong order")
-                    
-                    item["keywords"] = keyword_list
+                print(f"DONE IN {int(time.time() - start_time)}s")
 
-                return list_data
+                return list(post_by_id_dict.values())
             
             except ResourceExhausted as re:
                 TerminalLogging.log_error(f"API KEY {api_key} is exhausted. Wait and retry...")
@@ -60,4 +69,7 @@ class KeywordExtractionUtils():
                 continue
             except KeywordsNotMatchedException as ke:
                 TerminalLogging.log_error(f"{ke.msg}. Retrying...")
+                continue
+            except (json.decoder.JSONDecodeError, KeyError) as ge:
+                TerminalLogging.log_error(f"Model returns wrong format/data")
                 continue
