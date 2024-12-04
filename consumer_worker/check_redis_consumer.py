96ebef7d55bc29b8d4550f98fd8d5219234a1bf1
Vu Minh Tuan <tuanvm@dagoras.io>
2024-12-04 23:33:15 +0700

    redis checker insert keywords

--- consumer_worker/check_redis_consumer.py
+++ consumer_worker/check_redis_consumer.py
@@ -2,11 +2,14 @@ from connectors.db_connector import KafkaConsumerBuilder, DbConnectorBuilder
 from utils.constants import KafkaConnectionConstant as Kafka, SchemaPathConstant as Schema, RedisConnectionConstant as RedisCons
 from datetime import datetime
 from custom_logging.logging import TerminalLogging
+import traceback
+from time import sleep
 
-class ConsumerWorker():
+class CheckRedisConsumer():
     def __init__(self) -> None:
         self.kafka_consumer = KafkaConsumerBuilder().set_brokers(Kafka.BROKERS)\
                                                     .set_group_id(Kafka.GROUP_ID_CACHE_CHECKER)\
+                                                    .set_auto_offset_reset("earliest")\
                                                     .set_topics(Kafka.TOPIC_PARSED_POST)\
                                                     .build(avro_schema_path=Schema.PARSED_POST_SCHEMA)
         self.redis_conn = DbConnectorBuilder().set_host(RedisCons.HOST)\
@@ -20,6 +23,7 @@ class ConsumerWorker():
         list_keys = set()
         dict_keyword_post = dict()
 
+        TerminalLogging.log_info(f"Inserting into redis...")
         for d in docs:
             post_id = d.get("id")
             post_time = datetime.strptime(d["post_time"], "%Y-%m-%d %H:%M:%S")
@@ -29,24 +33,33 @@ class ConsumerWorker():
                 continue
 
             for keyword in d.get("keywords"):
+                if keyword == "":
+                    continue
                 if dict_keyword_post.get(keyword) == None:
                     dict_keyword_post[keyword] = [post_id]
                 else:
                     dict_keyword_post[keyword].append(post_id)
 
-        value = ""
-        pipeline = self.redis_conn.pipeline()
+        try:
+            value = ""
+            pipeline = self.redis_conn.pipeline()
 
-        for key in list_keys:
-            pipeline.set(key, value, ex=ttl_id)
+            for key in list_keys:
+                pipeline.set(key, value, ex=ttl_id)
 
-        for keyword in dict_keyword_post.keys():
-            corresponding_posts = dict_keyword_post.get(keyword)
-            pipeline.sadd(keyword, *corresponding_posts)
-            pipeline.expire(keyword, ttl_keyword)
+            for keyword in dict_keyword_post.keys():
+                corresponding_posts = dict_keyword_post.get(keyword)
+                keyword_keyname = f"{RedisCons.PREFIX_KEYWORD}.{keyword}"
+                pipeline.sadd(keyword_keyname, *corresponding_posts)
+                pipeline.expire(keyword_keyname, ttl_keyword)
 
-        pipeline.execute()
-        pipeline.close()
+            pipeline.execute()
+            pipeline.close()
+
+            TerminalLogging.log_info(f"Done inserting into redis!")
+        except Exception as e:
+            TerminalLogging.log_error(traceback.format_exc())
+            sleep(10000)
 
     def start(self, max_records=50):
         post_list = []
