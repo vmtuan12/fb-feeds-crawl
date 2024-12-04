5ac1c88089b23ab974ea0ac0007542b77726c1e5
Vu Minh Tuan <tuanvm@dagoras.io>
2024-12-04 15:25:36 +0700

    update parse post time

--- utils/parser_utils.py
+++ utils/parser_utils.py
@@ -19,47 +19,42 @@ class ParserUtils():
         
         return None
     @classmethod
-    def match_time(cls, text: str) -> str | None:
+    def match_time(cls, text: str) -> list:
         unescaped_text = html.unescape(text)
         pattern = r'[0-9a-zA-Z ,\.]+'
         matches = re.findall(pattern, unescaped_text)
-        if matches:
-            pos = -1
-            while (pos >= (len(matches) * (-1))):
-                res = matches[pos].strip()
-                if res != "":
-                    return res
-                pos -= 1
-
-        return None
+        return matches
     
     @classmethod
     def approx_post_time_str(cls, now: datetime, raw_post_time: str) -> str | None:
-        post_time = cls.match_time(raw_post_time)
-        if post_time == None:
-            return None
-        
-        if post_time.startswith("Jus"):
-            return now.strftime("%Y-%m-%d %H:%M:%S")
+        post_time_matches = cls.match_time(raw_post_time)
+        for post_time in post_time_matches:
+            try:
+                if post_time.startswith("Jus"):
+                    return now.strftime("%Y-%m-%d %H:%M:%S")
 
-        try:
-            post_time_formatted = post_time if (',' in post_time) else f"{post_time}, {datetime.now().year}"
-            real_post_time = datetime.strptime(post_time_formatted, "%b %d, %Y").strftime("%Y-%m-%d %H:%M:%S")
-            return real_post_time
-        except Exception as e:
-            real_post_time = None
+                try:
+                    post_time_formatted = post_time if (',' in post_time) else f"{post_time}, {datetime.now().year}"
+                    real_post_time = datetime.strptime(post_time_formatted, "%b %d, %Y").strftime("%Y-%m-%d %H:%M:%S")
+                    return real_post_time
+                except Exception as e:
+                    real_post_time = None
 
-        time_int = int(post_time[:-1])
-        unit = post_time[-1]
+                time_int = int(post_time[:-1])
+                unit = post_time[-1]
 
-        if unit == "m" or unit == "p":
-            real_post_time = now - timedelta(minutes=time_int)
-        elif unit == "h":
-            real_post_time = now - timedelta(hours=time_int)
-        elif unit == "d":
-            real_post_time = now - timedelta(days=time_int)
+                if unit == "m" or unit == "p":
+                    real_post_time = now - timedelta(minutes=time_int)
+                elif unit == "h":
+                    real_post_time = now - timedelta(hours=time_int)
+                elif unit == "d":
+                    real_post_time = now - timedelta(days=time_int)
 
-        return real_post_time.strftime("%Y-%m-%d %H:%M:%S")
+                return real_post_time.strftime("%Y-%m-%d %H:%M:%S")
+            except Exception as e:
+                pass
+
+        return None
     
     @classmethod
     def approx_reactions(cls, raw_reactions: str) -> int:
